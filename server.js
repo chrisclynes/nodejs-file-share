9d53051346732dcd70e2d208b04794c82bfe980b
Chris Clynes <chrisclynes@live.com>
2022-08-09 15:47:04 -0400

    added bootstrap and styling, imported methods for file share handling, setup .evn

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const multer = require("multer")
+const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
+
+//set upload file path with multer
+//when called, this will create a folder in root called upload with the file
+//file will have a random generated name, no file name conflicts
+const upload = multer({ dest: "uploads"})
+
+mongoose.connect(process.env.DATABASE_URL)
+
+//setup link to styles
+app.use("/public", express.static('public'));
 
 app.set("view engine", "ejs")
 
@@ -8,5 +20,10 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+//on submit, before the request have multer stage the file for upload, takes file from form submit
+app.post("/upload", upload.single("file") (req, res) => {
+    res.send("file sent")
+})
+
 //listen on port 3000
-app.listen(3000)
+app.listen(process.env.PORT)
